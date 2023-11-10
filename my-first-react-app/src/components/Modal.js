1c354cf1e2b4e9ef8c1ce40cf6b2fae45114bfa1
André Mendes <andregmendes@gmail.com>
2023-11-10 16:26:30 +0000

    lesson-34 - Stylesheets

--- my-first-react-app/src/components/Modal.js
+++ my-first-react-app/src/components/Modal.js
@@ -5,6 +5,7 @@ export default function Modal({ children, handleClose }) {
   return ReactDOM.createPortal((
     <div className="modal-backdrop">
       <div className="modal">
+        <h3 /*style= {{color: "yellow"}}*/>I'm coming from the Model Component</h3>
         {children}
         <button onClick={handleClose}>Close</button>
       </div>
--- my-first-react-app/src/components/Title.js
+++ my-first-react-app/src/components/Title.js
@@ -4,6 +4,7 @@ export default function Title({ title, subtitle }) {
       <h1 className="title">{title}</h1>
       <br />
       <h2 className="subtitle">{subtitle}</h2>
+      <h3>I'm an H3 comming from the Component Title</h3> 
     </>
   )
 }
