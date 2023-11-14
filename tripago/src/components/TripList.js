7c8d248b078dee8b141741207b72d04dd745ce88
André Mendes <andregmendes@gmail.com>
2023-11-14 14:37:35 +0000

    fetch using useEffect

--- tripago/src/components/TripList.js
+++ tripago/src/components/TripList.js
@@ -1,28 +1,30 @@
-import { useState, useEffect } from 'react'
-import './TripList.css'
+import { useState, useEffect } from "react";
+import "./TripList.css";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([])
+  const [trips, setTrips] = useState([]);
 
-  console.log(trips)
+  console.log(trips);
 
   useEffect(() => {
-    fetch('http://localhost:3000/trips')
-      .then(response => response.json())
-      .then(json => setTrips(json))
-  }, [])
+    fetch("http://localhost:3000/trips")
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+  }, []);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
-        {trips.map(trip => (
-          <li key={trip.id}>
-            <h3>{trip.title}</h3>
-            <p>{trip.price}</p>
-          </li>
-        ))}
+        {trips.map((trip) => {
+          return (
+            <li key={trip.id}>
+              <h3>{trip.title}</h3>
+              <p>{trip.price}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
+  );
 }
